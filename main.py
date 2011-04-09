e66dabb16ce8c1adc80fd2c42f35d3a2f82c9ca5
Antigluk <antigluk@gmail.com>
2011-04-09 15:17:55 +0300

    Retrieving data moved to separated file DataRetriever.py, according to MVC

--- main.py
+++ main.py
@@ -2,72 +2,24 @@
 # -*- coding: utf-8 -*- 
 
 import wx
-import re
-
-class EUnsupported(Exception): pass
-
-# Interface:
-class InfoGetter(object):
-    def get_info(self, name):
-        pass
-    def get_all_info(self):
-        pass
-    
-class IGSys(InfoGetter):
-    def __init__(self, bat="BAT0"):
-        s = lambda f:(lambda:self._read_file("/sys/class/power_supply/"+bat+"/"+f))
-        self._params={"charging state" : s("status"),
-                      "present rate": s("current_now"),
-                      "present voltage": s("voltage_now"),
-                      "remaining capacity": s("charge_now")}
-        
-    def _read_file(self, f):
-        fl = open(f, "rb")
-        return fl.readline().strip()
-        
-    def get_info(self, name):
-        try:
-            return self._params[name]()
-        except KeyError:
-            raise EUnsupported(name+" is unsupported here")
-    
-    def get_all_info(self):
-        return dict(zip(self._params.keys(),map(lambda x: x(), self._params.values())))
-
-class IGProc(InfoGetter):
-    def __init__(self, bat="BAT0"):
-        self.file = "/proc/acpi/battery/"+bat+"/state"
-        
-    def get_info(self, name):
-        f = open(self.file, "rb")
-        l = f.readlines()
-        try:
-            return dict(map(lambda x:[x[1], x[2]], map(lambda m:re.split("(.*): *(.*)", m), l)))[name]
-        except KeyError:
-            raise EUnsupported(name+" is unsupported here")
-        f.close()
-        
-    def get_all_info(self):
-        f = open(self.file, "rb")
-        l = f.readlines()
-        return dict(map(lambda x:[x[1], x[2]], map(lambda m:re.split("(.*): *(.*)", m), l)))
-        f.close()
+import DataRetriever as dr
+import sys
 
 class Main(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, -1, "Battery state", style=wx.BORDER_DEFAULT, size=(240,120,))
         self.panel = wx.Panel(self, -1)
-        self._params = {"charging state" : "Charging state",
-                        "present rate" : "Present rate",
-                        "present voltage" : "Present voltage",
-                        "remaining capacity" : "Remaining capacity"}
+        self._params = {dr.BAT_CHARGINGSTATE : "Charging state",
+                        dr.BAT_PRESENTRATE : "Present rate",
+                        dr.BAT_PRESENTVOLTAGE : "Present voltage",
+                        dr.BAT_REMAININGCAPACITY : "Remaining capacity"}
         self._ys=[10,35,60,85]
         make_text_ctrl = lambda y: wx.TextCtrl(self.panel, -1, "", pos=(150, y), style=wx.TE_READONLY, size=(80,-1))
         make_static_text = lambda text, y: wx.StaticText(self.panel, -1, text, pos=(10, y))
         self._st = map(make_static_text, self._params.values(), self._ys)
         self._ed = dict(zip( self._params, map(make_text_ctrl, self._ys) ))
         #updater
-        self.info_getter = IGSys("BAT0")
+        self.info_getter = dr.IGSys("BAT0")
         self.fill_info(None)
         self.Bind(wx.EVT_TIMER, self.fill_info)
         self.timer = wx.Timer(self)
